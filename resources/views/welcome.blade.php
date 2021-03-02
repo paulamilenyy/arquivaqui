28a38e30ee266f372a34a53c7a02b6a228871ec5
paulamilenyy <paulamileny.always@gmail.com>
2021-03-02 15:18:31 -0300

    design da tela welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,76 +2,81 @@
 <html>
 
 <head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width initial-scale=1.0">
-	<title>Arquivaqui</title>
-	<!-- GLOBAL MAINLY STYLES-->
-	<link href="{{ asset('./assets/vendors/bootstrap/dist/css/bootstrap.min.css') }}" rel="stylesheet" />
-	<link href="{{ asset('./assets/vendors/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet" />
-	<link href="{{ asset('./assets/vendors/themify-icons/css/themify-icons.css') }}" rel="stylesheet" />
-	<!-- THEME STYLES-->
-	<link href="{{ asset('assets/css/main.css') }}" rel="stylesheet" />
-	<!-- PAGE LEVEL STYLES-->
-	<link href="{{ asset('./assets/css/pages/auth-light.css') }}" rel="stylesheet" />
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width initial-scale=1.0">
+    <title>Arquivaqui</title>
+    <!-- GLOBAL MAINLY STYLES-->
+    <link href="{{ asset('./assets/vendors/bootstrap/dist/css/bootstrap.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('./assets/vendors/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('./assets/vendors/themify-icons/css/themify-icons.css') }}" rel="stylesheet" />
+    <!-- THEME STYLES-->
+    <link href="{{ asset('assets/css/main.css') }}" rel="stylesheet" />
+    <!-- PAGE LEVEL STYLES-->
+    <link href="{{ asset('./assets/css/pages/auth-light.css') }}" rel="stylesheet" />
 </head>
 <style type="text/css">
-	.content{
-		position: fixed;
-		top: 50%;
-		left: 50%;
-		/* bring your own prefixes */
-		transform: translate(-50%, -50%);
-	}
+.content {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    /* bring your own prefixes */
+    transform: translate(-50%, -50%);
+}
 </style>
 
 <body class="bg-silver-300" style="background-image:url({{url('./fundo2.jpg')}}); background-repeat: no-repeat;">
-	<div class="content" >
-		<div class="card-body" style="background-color: white;">
-	Bem vindo ao sistema Arquivaqui!
-	Faça seu login e continue
-	login
-	<a href="{{route('cheques.index')}}">Login  
-	</a> 
-</div>
-	</div>
-	<!-- BEGIN PAGA BACKDROPS-->
-	<div class="sidenav-backdrop backdrop"></div>
-	<div class="preloader-backdrop">
-		<div class="page-preloader">Loading</div>
-	</div>
-	<!-- END PAGA BACKDROPS-->
-	<!-- CORE PLUGINS -->
-	<script src="{{ asset('./assets/vendors/jquery/dist/jquery.min.js') }}" type="text/javascript"></script>
-	<script src="{{ asset('./assets/vendors/popper.js/dist/umd/popper.min.js') }}" type="text/javascript"></script>
-	<script src="{{ asset('./assets/vendors/bootstrap/dist/js/bootstrap.min.js') }}" type="text/javascript"></script>
-	<!-- PAGE LEVEL PLUGINS -->
-	<script src="{{ asset('./assets/vendors/jquery-validation/dist/jquery.validate.min.js') }}" type="text/javascript"></script>
-	<!-- CORE SCRIPTS-->
-	<script src="{{ asset('assets/js/app.js') }}" type="text/javascript"></script>
-	<!-- PAGE LEVEL SCRIPTS-->
-	<script type="text/javascript">
-		$(function() {
-			$('#login-form').validate({
-				errorClass: "help-block",
-				rules: {
-					email: {
-						required: true,
-						email: true
-					},
-					password: {
-						required: true
-					}
-				},
-				highlight: function(e) {
-					$(e).closest(".form-group").addClass("has-error")
-				},
-				unhighlight: function(e) {
-					$(e).closest(".form-group").removeClass("has-error")
-				},
-			});
-		});
-	</script>
+
+    <div class="content">
+        <img class="card-img-top" src="./assets/img/logoarquivaqui.jpeg" />
+        <div class="card-body" style="background-color: white;">
+            <h2>Bem-vindx ao sistema Arquivaqui!
+            </h2>
+            <h2>Faça seu login e continue</h2>
+            <button class="btn btn-primary"><a style="color:white;" href="{{route('cheques.index')}}">Login
+                </a></button>
+            <!--Com isso vai ir para a tela de login, pois para acessar o cheque tem q estar logado-->
+        </div>
+    </div>
+    <!-- BEGIN PAGA BACKDROPS-->
+    <div class="sidenav-backdrop backdrop"></div>
+    <div class="preloader-backdrop">
+        <div class="page-preloader">Loading</div>
+    </div>
+    <!-- END PAGA BACKDROPS-->
+    <!-- CORE PLUGINS -->
+    <script src="{{ asset('./assets/vendors/jquery/dist/jquery.min.js') }}" type="text/javascript">
+    </script>
+    <script src="{{ asset('./assets/vendors/popper.js/dist/umd/popper.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('./assets/vendors/bootstrap/dist/js/bootstrap.min.js') }}" type="text/javascript"></script>
+    <!-- PAGE LEVEL PLUGINS -->
+    <script src="{{ asset('./assets/vendors/jquery-validation/dist/jquery.validate.min.js') }}" type="text/javascript">
+    </script>
+    <!-- CORE SCRIPTS-->
+    <script src="{{ asset('assets/js/app.js') }}" type="text/javascript"></script>
+    <!-- PAGE LEVEL SCRIPTS-->
+    <script type="text/javascript">
+    $(function() {
+        $('#login-form').validate({
+            errorClass: "help-block",
+            rules: {
+                email: {
+                    required: true,
+                    email: true
+                },
+                password: {
+                    required: true
+                }
+            },
+            highlight: function(e) {
+                $(e).closest(".form-group").addClass("has-error")
+            },
+            unhighlight: function(e) {
+                $(e).closest(".form-group").removeClass("has-error")
+            },
+        });
+    });
+    </script>
 </body>
 
 </html>
