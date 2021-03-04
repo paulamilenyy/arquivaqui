067d85c9517b3b84d403bf90372e6f92b81abf7e
paulamilenyy <paulamileny.always@gmail.com>
2021-03-03 21:08:59 -0300

    design tela welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,20 +1,6 @@
 <!DOCTYPE html>
 <html>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width initial-scale=1.0">
-    <title>Arquivaqui</title>
-    <!-- GLOBAL MAINLY STYLES-->
-    <link href="{{ asset('./assets/vendors/bootstrap/dist/css/bootstrap.min.css') }}" rel="stylesheet" />
-    <link href="{{ asset('./assets/vendors/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet" />
-    <link href="{{ asset('./assets/vendors/themify-icons/css/themify-icons.css') }}" rel="stylesheet" />
-    <!-- THEME STYLES-->
-    <link href="{{ asset('assets/css/main.css') }}" rel="stylesheet" />
-    <!-- PAGE LEVEL STYLES-->
-    <link href="{{ asset('./assets/css/pages/auth-light.css') }}" rel="stylesheet" />
-</head>
 <style type="text/css">
 .content {
     position: fixed;
@@ -22,10 +8,32 @@
     left: 50%;
     /* bring your own prefixes */
     transform: translate(-50%, -50%);
+    box-shadow: 0 12px 16px 0 rgba(0, 0, 0, 0.24), 0 17px 50px 0 rgba(0, 0, 0, 0.19);
+}
+
+.button {
+    background-color: darkcyan;
+    /* Green */
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+    -webkit-transition-duration: 0.4s;
+    /* Safari */
+    transition-duration: 0.4s;
+}
+
+.button2:hover {
+    box-shadow: 0 12px 16px 0 rgba(0, 0, 0, 0.24), 0 17px 50px 0 rgba(0, 0, 0, 0.19);
 }
 </style>
 
-<body class="bg-silver-300" style="background-image:url({{url('./fundo2.jpg')}}); background-repeat: no-repeat;">
+<body style="background-image:url({{url('./fundo2.jpg')}}); background-repeat: no-repeat;">
 
     <div class="content">
         <!--<img class="card-img-top" src="./assets/img/logoarquivaqui.jpeg" />-->
@@ -33,51 +41,13 @@
             <h2>Bem-vindx ao sistema Arquivaqui!
             </h2>
             <h2>Faça seu login e continue</h2>
-            <button class="btn btn-primary">
+            <button class="button button2">
                 <a style="color:white;" href="{{route('cheques.index')}}">Login
                 </a></button>
             <!--Com isso vai ir para a tela de login, pois para acessar o cheque tem q estar logado-->
         </div>
     </div>
-    <!-- BEGIN PAGA BACKDROPS-->
-    <div class="sidenav-backdrop backdrop"></div>
-    <div class="preloader-backdrop">
-        <div class="page-preloader">Loading</div>
-    </div>
-    <!-- END PAGA BACKDROPS-->
-    <!-- CORE PLUGINS -->
-    <script src="{{ asset('./assets/vendors/jquery/dist/jquery.min.js') }}" type="text/javascript">
-    </script>
-    <script src="{{ asset('./assets/vendors/popper.js/dist/umd/popper.min.js') }}" type="text/javascript"></script>
-    <script src="{{ asset('./assets/vendors/bootstrap/dist/js/bootstrap.min.js') }}" type="text/javascript"></script>
-    <!-- PAGE LEVEL PLUGINS -->
-    <script src="{{ asset('./assets/vendors/jquery-validation/dist/jquery.validate.min.js') }}" type="text/javascript">
-    </script>
-    <!-- CORE SCRIPTS-->
-    <script src="{{ asset('assets/js/app.js') }}" type="text/javascript"></script>
-    <!-- PAGE LEVEL SCRIPTS-->
-    <script type="text/javascript">
-    $(function() {
-        $('#login-form').validate({
-            errorClass: "help-block",
-            rules: {
-                email: {
-                    required: true,
-                    email: true
-                },
-                password: {
-                    required: true
-                }
-            },
-            highlight: function(e) {
-                $(e).closest(".form-group").addClass("has-error")
-            },
-            unhighlight: function(e) {
-                $(e).closest(".form-group").removeClass("has-error")
-            },
-        });
-    });
-    </script>
+
 </body>
 
 </html>
