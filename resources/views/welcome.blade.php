d67f3889dc5c7d5f734d609ab1beaa307e0c1749
paulamilenyy <paulamileny.always@gmail.com>
2021-03-07 20:43:28 -0300

    design da tela welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,53 +1,33 @@
+
 <!DOCTYPE html>
 <html>
-
-<style type="text/css">
-.content {
-    position: fixed;
-    top: 50%;
-    left: 50%;
-    /* bring your own prefixes */
-    transform: translate(-50%, -50%);
-    box-shadow: 0 12px 16px 0 rgba(0, 0, 0, 0.24), 0 17px 50px 0 rgba(0, 0, 0, 0.19);
-}
-
-.button {
-    background-color: darkcyan;
-    /* Green */
-    border: none;
-    color: white;
-    padding: 15px 32px;
-    text-align: center;
-    text-decoration: none;
-    display: inline-block;
-    font-size: 16px;
-    margin: 4px 2px;
-    cursor: pointer;
-    -webkit-transition-duration: 0.4s;
-    /* Safari */
-    transition-duration: 0.4s;
-}
-
-.button2:hover {
-    box-shadow: 0 12px 16px 0 rgba(0, 0, 0, 0.24), 0 17px 50px 0 rgba(0, 0, 0, 0.19);
+<title>Arquivaqui</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="{{asset(  'https://www.w3schools.com/w3css/4/w3.css')}}">
+<link rel="stylesheet" href="{{asset('https://fonts.googleapis.com/css?family=Raleway')}}">
+<style>
+body,h1 {font-family: "Raleway", sans-serif}
+body, html {height: 100%}
+.bgimg {
+  background-image: url({{url('./fundo2.jpg')}});
+  min-height: 100%;
+  background-position: center;
+  background-size: cover;
 }
 </style>
+<body>
 
-<body style="background-image:url({{url('./fundo2.jpg')}}); background-repeat: no-repeat;">
-
-    <div class="content">
-        <!--<img class="card-img-top" src="./assets/img/logoarquivaqui.jpeg" />-->
-        <div class="card-body" style="background-color: white;">
-            <h2>Bem-vindx ao sistema Arquivaqui!
-            </h2>
-            <h2>Faça seu login e continue</h2>
-            <button class="button button2">
-                <a style="color:white;" href="{{route('cheques.index')}}">Login
-                </a></button>
-            <!--Com isso vai ir para a tela de login, pois para acessar o cheque tem q estar logado-->
-        </div>
-    </div>
+<div class="bgimg w3-display-container w3-animate-opacity w3-text-white">
+  
+  <div class="w3-display-middle">
+    <h1 class="w3-jumbo w3-animate-top"><a style="color:white;" href="{{route('cheques.index')}}">Login
+                </a></h1>
+    <hr class="w3-border-grey" style="margin:auto;width:40%">
+    <p class="w3-large w3-center"> Faça seu login e continue</p>
+  </div>
+ 
+</div>
 
 </body>
-
 </html>
